06d982b6e003e7981107c874e98ceb0c722141b5
Matthew Aitken <matt@itken.net>
2024-09-27 11:17:06 +0000

    Set auto DEBUG

--- project_one/settings.py
+++ project_one/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+development = os.environ.get('DEVELOPMENT', False)
+DEBUG = development
 
 ALLOWED_HOSTS = [
     '.ws.codeinstitute-ide.net',
