47cedc3839aa4ea618d8d774d21676ba10bb09c9
Matthew Aitken <matt@itken.net>
2024-09-27 08:13:58 +0000

    Initial commit

--- app_one/asgi.py
+++ app_one/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for training project.
+ASGI config for app_one project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'training.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_one.settings')
 
 application = get_asgi_application()
--- x_app_1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class XApp1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'x_app_1'
