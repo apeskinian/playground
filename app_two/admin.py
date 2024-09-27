7ce0258054f28551072ab77f3dc15d4ab8c1a531
Matthew Aitken <matt@itken.net>
2024-09-27 13:46:44 +0000

    Tidy admin panel entries

--- app_two/admin.py
+++ app_two/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
+from allauth.account.models import EmailAddress
+from django.contrib.sites.models import Site
 from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.utils import get_attachment_model 
 from .models import Watch
 
 @admin.register(Watch)
 class WatchAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('make','collection','model',)}
+
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(EmailAddress)
+admin.site.unregister(Site)
+admin.site.unregister(get_attachment_model())
--- project_one/urls.py
+++ project_one/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path("accounts/", include("allauth.urls")),
 ]
+
